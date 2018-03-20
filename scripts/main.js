53d165b72506907133bdd4be3b64110a7e602e05
Ching-An <chingan@ChingandeMacBook-Air.local>
2018-03-20 16:40:50 -0700

    Finished.

--- scripts/main.js
+++ scripts/main.js
@@ -43,14 +43,14 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  myHeading.innerHTML = 'Lets use Foodle, ' + myName + '!';
 }
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = 'Lets use Foodle, ' + storedName + '!';
 }
 
 myButton.onclick = function() {
